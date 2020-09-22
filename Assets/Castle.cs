ba7ef76538f31bb7597e11722270283d6ebc2e03
izzy <44009748+izzyphobe@users.noreply.github.com>
2020-09-22 17:07:05 -0500

    Basic level outline + spawner stuff

---
+++ Assets/Castle.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Castle : MonoBehaviour
+{
+    [SerializeField] private LayerMask groundMask;
+    private Rigidbody2D rb;
+    private BoxCollider2D bc;
+    [SerializeField] private float rate;
+   [SerializeField] private float delay;
+    private int minionCap;
+    public GameObject prefab;
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        minionCap = 5;
+        InvokeRepeating("Spawn",delay,rate);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Spawn(){
+        GameObject child;
+        if(transform.childCount < minionCap){
+            child = Instantiate(prefab, transform);
+            child.tag = this.tag;
+        }
+        
+    }
+}
---
+++ Assets/Minion.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minion : MonoBehaviour
+{
+    [SerializeField] private LayerMask groundMask;
+    private Rigidbody2D rb;
+    private CircleCollider2D cc;
+
+    public Transform target;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.Find("Battlefield").transform;
+        rb = GetComponent<Rigidbody2D>();
+        cc = GetComponent<CircleCollider2D>();
+        //initially - target = "battle area" in center
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+        //Attack target if target in range
+    //Move to target if out of range and alive
+    //Find new target (vector.movetowards)
+    //Find target, move towards target
+    //If someone attacks them or is within a certain range, that person becomes their target
+    //If can't find minion to target, start attacking spawner
+    //
+        
+    }
+
+    void FixedUpdate(){
+        //move toward target
+        transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+    }
+
+    void Attack(){
+
+    }
+
+    void SetTarget(GameObject t){ //change target to t
+
+    }
+
+}
