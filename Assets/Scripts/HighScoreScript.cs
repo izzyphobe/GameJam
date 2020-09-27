0132bff27efba9d3aad0997fe28105cd568f327c
Max Tribble <mtribble@trinity.edu>
2020-09-27 18:44:11 -0500

    finished startMenu

--- Assets/Scripts/HighScoreScript.cs
+++ Assets/Scripts/HighScoreScript.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class HighScoreScript : MonoBehaviour
 {
+    TMPro.TextMeshProUGUI text;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        text = GetComponent<TMPro.TextMeshProUGUI>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        text.text = "Score: " + PlayerPrefs.GetInt("HighScore", 0);
     }
 }
